af0ba83213d861a0f4d142e99d1ce56c5cb7ee07
wanghonglei <836612526@qq.com>
2018-10-23 14:10:58 +0800

    state props style

--- App.js
+++ App.js
@@ -6,31 +6,70 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
 });
+
+class Greeting extends Component {
+  render() {
+    return (
+      <View style={{ alignItems: 'center' }}>
+        <Text>Hello {this.props.name}</Text>
+      </View>
+    );
+  }
+}
+
+class Blink extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { showText: true }
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText }
+      });
+    }, 1000)
+  }
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    )
+  }
+}
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    let pic = {
+      uri: 'https://wx2.sinaimg.cn/mw690/5fe93731gy1fw98zcemqlj20ku0ku76e.jpg'
+    };
     return (
       <View style={styles.container}>
+        <Blink text='bling bling' />
         <Text style={styles.hello}>Hello World!</Text>
-		<Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Image source={pic} style={{ width: 190, height: 200 }} />
+        <Greeting name='liuqiang' />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  hello: {
+    color: '#ff0000',
+    fontWeight: 'bold',
+    fontSize: 28,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
