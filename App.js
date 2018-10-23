d178ed34b7949eb12459e98043c1fbd37746f071
wanghonglei <836612526@qq.com>
2018-10-23 10:39:12 +0800

    修改图标

--- App.js
+++ App.js
@@ -21,7 +21,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.hello}>Hello World!</Text>
+		<Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
